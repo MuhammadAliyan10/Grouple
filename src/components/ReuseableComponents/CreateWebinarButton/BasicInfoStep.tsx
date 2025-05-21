c971e836843d57f20f62261bd87960d06f4b4de5
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2025-05-21 23:54:44 +0500

    Webniar step half complete by 80%

--- src/components/ReuseableComponents/CreateWebinarButton/BasicInfoStep.tsx
+++ src/components/ReuseableComponents/CreateWebinarButton/BasicInfoStep.tsx
@@ -1,9 +1,135 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useWebinarStore } from "@/store/useWebinarStore";
+import { cn } from "@/lib/utils";
 import React from "react";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+
+import { Calendar } from "@/components/ui/calendar";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { toast } from "sonner";
 
 type Props = {};
 
 const BasicInfoStep = (props: Props) => {
-  return <div>BasicInfoStep</div>;
+  const { formData, resetForm, getStepValidationErrors, updateBasicInfoField } =
+    useWebinarStore();
+  const errors = getStepValidationErrors("basicInfo");
+  const { webinarName, description, date, time, timeFormat } =
+    formData.basicInfo;
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    updateBasicInfoField(name as keyof typeof formData.basicInfo, value);
+  };
+  const handleDateChange = (newDate: Date | undefined) => {
+    if (newDate) {
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      if (newDate < today) {
+        toast.error("Webinar date cannot be past.");
+      }
+      updateBasicInfoField("date", newDate);
+    }
+  };
+  return (
+    <div className="space-y-6">
+      <div className="space-y-2">
+        <Label
+          htmlFor="webinarName"
+          className={errors.webinarName ? "text-red-400" : ""}
+        >
+          Webinar name <span className="text-red-400">*</span>
+        </Label>
+        {errors.webinarName && (
+          <p className="text-sm text-red-400">{errors.webinarName}</p>
+        )}
+        <Input
+          id="webinarName"
+          name="webinarName"
+          value={webinarName || ""}
+          onChange={handleChange}
+          placeholder="Introduction to Mochi"
+          className={cn(
+            "bg-background/50 border border-input",
+            errors.webinarName && "border-red-400 focus-visible:ring-red-400"
+          )}
+        />
+      </div>
+      <div className="space-y-2">
+        <Label
+          htmlFor="description"
+          className={errors.description ? "text-red-400" : ""}
+        >
+          Webinar description <span className="text-red-400">*</span>
+        </Label>
+        {errors.description && (
+          <p className="text-sm text-red-400">{errors.description}</p>
+        )}
+        <Textarea
+          id="description"
+          name="description"
+          value={description || ""}
+          onChange={handleChange}
+          placeholder="Introduction to Mochi"
+          className={cn(
+            "bg-background/50 border border-input",
+            errors.description && "border-red-400 focus-visible:ring-red-400"
+          )}
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="space-y-2">
+          <Label className={errors.date ? "text-red-400" : ""}>
+            Webinar Date <span className="text-red-400">*</span>
+          </Label>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-full justify-start text-left font-normal !bg-background/50 border border-input",
+                  !date && "text-gray-500",
+                  errors.date && "border-red-400 focus-visible:ring-red-400"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date ? format(date, "PPP") : "Select date"}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0 !bg-background/50 border border-input">
+              <Calendar
+                mode={"single"}
+                selected={date}
+                onSelect={handleDateChange}
+                initialFocus
+                className="bg-background"
+                disabled={(date) => {
+                  const today = new Date();
+                  today.setHours(0, 0, 0, 0);
+                  return date < today;
+                }}
+              />
+            </PopoverContent>
+          </Popover>
+          {errors.date && <p className="text-sm text-red-400">{errors.date}</p>}
+        </div>
+        <div className="space-y-2">
+          <Label className={errors.time ? "text-red-400" : ""}>
+            Webinar time <span className="text-red-400">*</span>
+          </Label>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BasicInfoStep;
--- src/components/ReuseableComponents/CreateWebinarButton/MultiStepForm.tsx
+++ src/components/ReuseableComponents/CreateWebinarButton/MultiStepForm.tsx
@@ -1,8 +1,13 @@
 import { useWebinarStore } from "@/store/useWebinarStore";
 import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { Check } from "lucide-react";
+import { AlertCircle, Check, ChevronRight, Loader2 } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { createWebinar } from "@/app/actions/webinar";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 type Step = {
   id: string;
   title: string;
@@ -15,10 +20,57 @@ type Props = {
 };
 
 const MultiStepForm = ({ steps, onComplete }: Props) => {
-  const {} = useWebinarStore();
+  const { isSubmitting, setModelOpen, validateStep, formData, setSubmitting } =
+    useWebinarStore();
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
   const [completedSteps, setCompletedSteps] = useState<string[]>([]);
   const [validationError, setValidationError] = useState<string | null>("");
+  const router = useRouter();
+
+  const handleBack = () => {
+    if (isFirstStep) {
+      setModelOpen(false);
+    } else {
+      setCurrentStepIndex(currentStepIndex - 1);
+      setValidationError(null);
+    }
+  };
+  const handleNext = async () => {
+    setValidationError(null);
+    const isValid = validateStep(currentStep.id as keyof typeof formData);
+    if (!isValid) {
+      setValidationError("Please fill in all required fields");
+      return;
+    }
+
+    if (!completedSteps.includes(currentStep.id)) {
+      setCompletedSteps([...completedSteps, currentStep.id]);
+    }
+    if (isLastStep) {
+      try {
+        setSubmitting(true);
+        const result = await createWebinar(formData);
+        if (result.status === 200 && result.webinarId) {
+          toast.success("Your webinar has created successfully.");
+          onComplete(result.webinarId);
+        } else {
+          toast.error(
+            result.message || "Your webinar has not been created successfully"
+          );
+          setValidationError(result.message);
+        }
+        router.refresh();
+      } catch (error) {
+        console.error("Error creating webinar", error);
+        toast.error("Failed to create webinar. Please try again.");
+        setValidationError("Failed to create webinar. Please try again.");
+      } finally {
+        setSubmitting(false);
+      }
+    } else {
+      setCurrentStepIndex(currentStepIndex + 1);
+    }
+  };
 
   const currentStep = steps[currentStepIndex];
   const isFirstStep = currentStepIndex === 0;
@@ -113,7 +165,57 @@ const MultiStepForm = ({ steps, onComplete }: Props) => {
           orientation="vertical"
           className="data-[orientation-vertical]:h-1/2"
         />
-        <div className="w-full md:w:2/3"></div>
+        <div className="w-full md:w:2/3">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={currentStep.id}
+              initial={{ x: 20, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: -20, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className="p-6"
+            >
+              <div className="mb-6">
+                <h2 className="text-xl font-semibold">{currentStep.title}</h2>
+                <p className="text-gray-400">{currentStep.description}</p>
+              </div>
+              {currentStep.component}
+              {validationError && (
+                <div className="mt-4 p-3 bg-red-900/30 border border-red-800 rounded-md flex items-center gap-2 text-red-300">
+                  <AlertCircle className="h-5 w-5 mt-0.5 flex-shrink-0" />
+                  <p className="">{validationError}</p>
+                </div>
+              )}
+            </motion.div>
+          </AnimatePresence>
+        </div>
+      </div>
+      <div className="w-full p-6 flex justify-between">
+        <Button
+          variant={"outline"}
+          onClick={handleBack}
+          disabled={isSubmitting}
+          className={cn(
+            "border-gray-700 text-white hover:bg-gray-800",
+            isFirstStep && "opacity-50 cursor-not-allowed"
+          )}
+        >
+          {isFirstStep ? "Cancel" : "Back"}
+        </Button>
+        <Button onClick={handleNext} disabled={isSubmitting}>
+          {isLastStep ? (
+            isSubmitting ? (
+              <>
+                <Loader2 className="animate-spin" /> Creating...
+              </>
+            ) : (
+              "Completed"
+            )
+          ) : (
+            "Next"
+          )}
+          {!isLastStep && <ChevronRight className="ml-1 h-4 w-4" />}
+        </Button>
       </div>
     </div>
   );
